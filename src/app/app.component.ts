80bc70c38f6fc79c0c65b0b3922b2c9c2108a086
Samuel <tobisamuel88@gmail.com>
2017-09-08 14:10:53 +0100

    landing page v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+//import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title: string = 'GatePassNG';
+  private sliderImages: any = ['assets/img/1.png', 'assets/img/2.png', 'assets/img/1.jpg', 'assets/img/4.jpg', 'assets/img/5.jpg', 'assets/img/6.jpg'];
+  private image: string = this.sliderImages[2];
+  private iosApp: string = 'assets/img/iosapp.png';
+  private ribbon: string = 'assets/img/ribbon.png';
+  private appstore: string = 'assets/img/appstore.png';
+  private playstore: string = 'assets/img/playstore.png';
+
+
+  constructor (){
+      //Observable.interval(3000).subscribe(() => {
+          //this.slideShow();
+      //});
+    }
+
 }
