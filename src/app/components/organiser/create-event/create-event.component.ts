bb633531a66d179ecba03661d92ec5fedf08d74c
Samuel <tobisamuel88@gmail.com>
2017-09-15 15:55:56 +0100

    create-event component added

---
+++ src/app/components/organiser/create-event/create-event.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-event',
+  templateUrl: './create-event.component.html',
+  styleUrls: ['./create-event.component.css']
+})
+export class CreateEventComponent implements OnInit {
+
+  mapTitle: string = 'google map';
+  position:any = {
+    'longitude': 4.673671399999989,
+    'latitude': 8.4793627
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => {
+        this.position = position.coords;
+        //console.log(this.position.longitude);
+        //console.log(this.position.latitude);
+      });
+    }
+  }
+
+}
