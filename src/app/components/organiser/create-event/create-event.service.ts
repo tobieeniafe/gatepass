ea1a9c460a39a9348996a5f28beab5dfc6d9671c
Samuel <tobisamuel88@gmail.com>
2017-09-22 00:56:37 +0100

    fixed date and timepicker initialization

--- src/app/components/organiser/create-event/create-event.service.ts
+++ src/app/components/organiser/create-event/create-event.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+declare var jQuery: any;
+declare var $: any;
 
 @Injectable()
 export class CreateEventService {
@@ -10,6 +12,37 @@ export class CreateEventService {
   constructor(public _http: Http) {
      this.http = _http;
      this.token = localStorage.getItem('token');
+     (function($){
+      $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+        $('.datepicker').pickadate({
+          format: 'dd/mm/yyyy',
+          formatSubmit: 'dd/mm/yyyy',
+          selectMonths: true, // Creates a dropdown to control month
+          selectYears: 10, // Creates a dropdown of 10 years to control year,
+          today: 'Today',
+          clear: 'Clear',
+          close: 'Ok',
+          closeOnSelect: false // Close upon selecting a date,
+        });
+
+        $('.timepicker').pickatime({
+            default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+            fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+            twelvehour: false, // Use AM/PM or 24-hour format
+            donetext: 'OK', // text for done-button
+            cleartext: 'Clear', // text for clear-button
+            canceltext: 'Cancel', // Text for cancel-button
+            autoclose: false, // automatic close timepicker
+            ampmclickable: true, // make AM PM clickable
+            aftershow: function(){} //Function for after opening timepicker
+          });
+
+      });
+    })(jQuery);
   }
 
   postEvent(event) {
@@ -22,5 +55,7 @@ export class CreateEventService {
         return res.json();
     });
   }
+
+  
 
 }
