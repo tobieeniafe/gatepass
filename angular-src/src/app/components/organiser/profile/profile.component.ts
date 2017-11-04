32d1361dfa6f6fef18a7481a6efaa7d8f7206f55
Samuel <tobisamuel88@gmail.com>
2017-11-05 00:14:23 +0100

    take it

--- angular-src/src/app/components/organiser/profile/profile.component.ts
+++ angular-src/src/app/components/organiser/profile/profile.component.ts
@@ -13,18 +13,20 @@ export class ProfileComponent implements OnInit {
 
   avatar: string = 'assets/img/avatar.jpg';
   user: any = {};
+  isDisabled: boolean = false;
   constructor(private profileService: ProfileService, private router: Router, private http: Http) { 
   	this.getUser()
   }
 
   ngOnInit() {}
 
+
   getUser(){
     this.profileService.getUser().subscribe(
        data => {
          this.user = data.organiser;
          if (data.status == true) { 
-          this.avatar = 'https://image.devidentify.com/'+this.user.email
+           this.avatar = 'https://image.devidentify.com/'+this.user.email
          } else {
          	 Materialize.toast("Something's not right", 1500, 'red white-text')
          }
@@ -34,22 +36,29 @@ export class ProfileComponent implements OnInit {
     );
   }
 
-  updateUser(){
+  updateUser(fn, pn, e, cp, np){
+    this.isDisabled = true
     let userDetails = {
-      fullname: this.user.fullname,
-      email: this.user.email,
-      phone: this.user.phone,
-      currentPassword: this.user.currentPassword,
-      newPassword: this.user.newPassword
+      fullname: fn,
+      email: e,
+      phone: pn,
+      currentPassword: cp,
+      newPassword: np
     }
-    console.log(userDetails)
-    // this.profileService.updateUser(userDetails).subscribe(
-    //    data => {
-    //      console.log(data)
-    //    },
-    //    err => Materialize.toast("Something's not right", 1500, 'red white-text'),
-    //    () => console.log()
-    // );
+    //console.log(userDetails)
+    this.profileService.updateUser(userDetails).subscribe(
+       data => {
+         if (data.status == true) { 
+           Materialize.toast("User updated", 1500, 'green white-text')
+           this.isDisabled = false
+         } else {
+           Materialize.toast("Error occured", 1500, 'red white-text')
+           this.isDisabled = false
+         }
+       },
+       err => Materialize.toast("Something's not right", 1500, 'red white-text'),
+       () => console.log()
+    );
   }
 
 }
